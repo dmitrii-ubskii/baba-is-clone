4bdb242e4bc617a7868506fce287b70f8ef8f39d
Dmitrii Ubskii <ubskydm@gmail.com>
2022-04-16 17:46:08 +0300

    level.gd: add rule parser

--- level.gd
+++ level.gd
@@ -2,10 +2,57 @@ extends Node2D
 
 var Entity = preload("res://sprites/Entity.tscn")
 
+var rules = []
+
+onready var cell_size = $TileMap.cell_size
+
 func _ready():
 	for tile in $TileMap.get_used_cells():
 		var entity = Entity.instance()
-		entity.position = tile * $TileMap.cell_size
+		entity.position = tile * cell_size
 		entity.entity = $TileMap.get_cellv(tile)
 		add_child(entity)
-	$TileMap.queue_free()
+	$TileMap.free()
+	_update_rules()
+
+func _update_rules():
+	rules.clear()
+	var text = {}
+	for entity in get_children():
+		match entity.entity:
+			EntityEnum.TEXT_BABA, \
+			EntityEnum.TEXT_FLAG, \
+			EntityEnum.TEXT_WALL, \
+			EntityEnum.PRED_STOP, \
+			EntityEnum.PRED_WIN, \
+			EntityEnum.PRED_YOU, \
+			EntityEnum.CONTROL_IS:
+				var cell = entity.position / cell_size
+				text[cell] = entity.entity
+	for pos in text:
+		match text[pos]:
+			EntityEnum.TEXT_BABA, \
+			EntityEnum.TEXT_FLAG, \
+			EntityEnum.TEXT_WALL:
+				if pos + Vector2.DOWN in text:
+					var next = pos + Vector2.DOWN
+					if text[next] == EntityEnum.CONTROL_IS:
+						next += Vector2.DOWN
+						if next in text:
+							match text[next]:
+								EntityEnum.PRED_STOP, \
+								EntityEnum.PRED_WIN, \
+								EntityEnum.PRED_YOU:
+									rules.append(Rule.new(text[pos], text[next]))
+				if pos + Vector2.RIGHT in text:
+					var next = pos + Vector2.RIGHT
+					if text[next] == EntityEnum.CONTROL_IS:
+						next += Vector2.RIGHT
+						if next in text:
+							match text[next]:
+								EntityEnum.PRED_STOP, \
+								EntityEnum.PRED_WIN, \
+								EntityEnum.PRED_YOU:
+									rules.append(Rule.new(text[pos], text[next]))
+	for rule in rules:
+		print(rule.format())
---
+++ rule.gd
@@ -0,0 +1,32 @@
+extends Object
+class_name Rule
+
+var subject = EntityEnum.TEXT_BABA
+var predicate = EntityEnum.PRED_YOU
+
+func _init(subj=EntityEnum.TEXT_BABA, pred=EntityEnum.PRED_YOU):
+	subject = subj
+	predicate = pred
+
+func format():
+	var rule_text = ""
+	match subject:
+		EntityEnum.TEXT_BABA:
+			rule_text += "Baba"
+		EntityEnum.TEXT_WALL:
+			rule_text += "Wall"
+		EntityEnum.TEXT_FLAG:
+			rule_text += "Flag"
+		_:
+			rule_text += "<ERROR>"
+	rule_text += " is "
+	match predicate:
+		EntityEnum.PRED_STOP:
+			rule_text += "Stop"
+		EntityEnum.PRED_WIN:
+			rule_text += "Win"
+		EntityEnum.PRED_YOU:
+			rule_text += "You"
+		_:
+			rule_text += "<ERROR>"
+	return rule_text
